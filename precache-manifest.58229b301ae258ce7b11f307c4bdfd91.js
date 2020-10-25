fca3a57d5bfa8f25ea12252a0a70a6368847f503
Ispas Valentin <51799997+ispasvalentin22@users.noreply.github.com>
2020-10-25 08:28:00 +0200

    Updates

---
+++ precache-manifest.58229b301ae258ce7b11f307c4bdfd91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38ff4e22b67c51d0e992650b36ad5096",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "70675d1a58cf15c5d11d",
+    "url": "/monsters-rolodex/static/css/main.23dd3b4f.chunk.css"
+  },
+  {
+    "revision": "0ebb722af33ec599140e",
+    "url": "/monsters-rolodex/static/js/2.c27b6d93.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/monsters-rolodex/static/js/2.c27b6d93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70675d1a58cf15c5d11d",
+    "url": "/monsters-rolodex/static/js/main.0f866938.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
